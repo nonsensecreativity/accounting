f7926d49813234058ef3b7ce365d8d565ae59090
Liam Demafelix <hello@liamdemafelix.com>
2015-12-22 14:16:42 +0800

    Fix alerts

--- resources/views/partials/alerts.blade.php
+++ resources/views/partials/alerts.blade.php
@@ -1,13 +1,13 @@
 @if (Session::has('success'))
     <div class="alert alert-success alert-dismissible" role="alert">
         <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        {{ Session::get('success') }}
+        {!! Session::get('success') !!}
     </div>
 @endif
 @if (Session::has('error'))
     <div class="alert alert-danger alert-dismissible" role="alert">
         <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        {{ Session::get('error') }}
+        {!! Session::get('error') !!}
     </div>
 @endif
 @if (count($errors) > 0)
@@ -15,7 +15,7 @@
         There were problems with your action:
         <ul>
             @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
+                <li>{!! $error !!}</li>
             @endforeach
         </ul>
     </div>
